60dbfce3c2adfd6638243a656a0f8b60102c3df9
sadikahasan6 <sadikahasan6@gmail.com>
2025-07-17 06:54:41 +0600

    init

--- src/routes/employee-info/+page.server.ts
+++ src/routes/employee-info/+page.server.ts
@@ -1,11 +1,41 @@
 import { db } from "$lib/server/db";
+import { employee } from "$lib/server/db/schema";
+import { redirect } from "@sveltejs/kit";
+import { eq } from "drizzle-orm";
 
-export async function load() {
-	try {
-		const employees = await db.query.employee.findMany();
-		return { employees };
-	} catch (error) {
-		console.error('Failed to load products:', error);
-		return { employees: [] };
-	}
+
+export async function load({ url }) {
+  const deleteId = url.searchParams.get('delete');
+  if (deleteId) {
+    await db.delete(employee).where(eq(employee.id, Number(deleteId)));
+    throw redirect(303, '/');
+  }
+  try {
+    const employees = await db.query.employee.findMany();
+    return { employees };
+
+  } catch (error) {
+    console.error('Failed to load products:', error);
+    return { employees: [] };
+  }
 }
+export const actions = {
+  default: async ({ request }) => {
+    const data = await request.formData();
+    const id = data.get('id');
+    const name = data.get('name') as string;
+    const nid = data.get('nid') as string;
+    const birthDate = data.get('birthDate') as string;
+
+    if (id) {
+      await db
+        .update(employee)
+        .set({ name, nid, birthDate })
+        .where(eq(employee.id, Number(id)));
+    } else {
+      await db.insert(employee).values({ name, nid, birthDate });
+    }
+
+    throw redirect(303, '/');
+  }
+};
--- src/routes/employee-info/+page.svelte
+++ src/routes/employee-info/+page.svelte
@@ -1,8 +1,17 @@
-<script>
+<script lang="ts">
 	import Header from '../../components/header.svelte';
+	import { enhance } from '$app/forms';
+	import Icon from '@iconify/svelte';
+	import { goto } from '$app/navigation';
 	export let data;
+
 	$: employees = data.employees;
-	console.log('Employee data:', employees);
+
+	let selectedEmployee: (typeof employees)[0] | null = null;
+
+	function handleRowClick(employee: any) {
+		selectedEmployee = employee;
+	}
 </script>
 
 <Header />
@@ -27,8 +36,8 @@
 				</thead>
 
 				<tbody>
-					{#each employees as employee (employee.id)}
-						<tr>
+					{#each employees as employee}
+						<tr style="cursor: pointer;" on:click={() => handleRowClick(employee)}>
 							<td>{employee.id}</td>
 							<td>{employee.name}</td>
 							<td>HR manager</td>
@@ -44,58 +53,89 @@
 							<td colspan="9">No employees found.</td>
 						</tr>
 					{/each}
-
-					<!-- <tr>
-						<td>01</td>
-						<td>Ahnaf Mir</td>
-						<td>HR manager</td>
-						<td>Human Resource</td>
-						<td>HR</td>
-						<td>10-Jan-2025</td>
-						<td>05-Nov-2002</td>
-						<td>10%</td>
-						<td>Present</td>
-					</tr> -->
 				</tbody>
 			</table>
 		</div>
 		<div class="input">
 			<h1 class="text-primary">Add Employee</h1>
-			<ul>
-				<li>
-					<label for="id">ID <span style="color:red">&nbsp; * &nbsp;</span>:</label>
-					<input id="id" type="text" />
-				</li>
-				<li><label for="name">Name:</label> <input id="name" type="text" /></li>
-				<li>
-					<label for="designataion">Designation:</label> <input id="designataion" type="text" />
-				</li>
-				<li><label for="department">Department:</label> <input id="department" type="text" /></li>
-				<li><label for="section">Section:</label> <input id="section" type="text" /></li>
-				<!-- <li><label for="father">Father's name:</label> <input id="father" type="text" /></li>
+			<form method="POST" use:enhance>
+				<ul>
+					<li>
+						<label for="id">ID <span style="color:red">&nbsp; * &nbsp;</span>:</label>
+						<input id="id" type="text" value={employees.length + 1} />
+					</li>
+					<li><label for="name">Name:</label> <input name="name" id="name" type="text" /></li>
+					<li>
+						<label for="designataion">Designation:</label> <input id="designataion" type="text" />
+					</li>
+					<li><label for="department">Department:</label> <input id="department" type="text" /></li>
+					<li><label for="section">Section:</label> <input id="section" type="text" /></li>
+					<!-- <li><label for="father">Father's name:</label> <input id="father" type="text" /></li>
 				<li><label for="mother">Mother's name:</label> <input id="mother" type="text" /></li> -->
-				<li>
-					<label for="birth-date">Date of Birth:</label> <input id="birth-date" type="date" />
-				</li>
-				<li><label for="Religion">Religion:</label> <input id="Religion" type="text" /></li>
-				<li><label for="gender">Gender:</label> <input id="gender" type="text" /></li>
-				<li><label for="nid">NID:</label> <input id="nid" type="text" /></li>
-				<li><label for="brc">BRC No.:</label> <input id="brc" type="text" /></li>
-				<li><label for="payment">Account type: </label> <input id="payment" type="text" /></li>
-				<li>
-					<label for="Account-no:">Account No.:</label> <input id="Account-no:" type="text" />
-				</li>
-				<!-- <li><label for="present-address">Present Address:</label> <input id="present-address" type="text" /></li>
+					<li>
+						<label for="birth-date">Date of Birth:</label>
+						<input name="birthDate" id="birth-date" type="date" />
+					</li>
+					<li><label for="Religion">Religion:</label> <input id="Religion" type="text" /></li>
+					<li><label for="gender">Gender:</label> <input id="gender" type="text" /></li>
+					<li><label for="nid">NID:</label> <input id="nid" type="text" /></li>
+					<li><label for="brc">BRC No.:</label> <input name="brc" id="brc" type="text" /></li>
+					<li><label for="payment">Account type: </label> <input id="payment" type="text" /></li>
+					<li>
+						<label for="Account-no:">Account No.:</label>
+						<input name="account" id="Account-no:" type="text" />
+					</li>
+					<!-- <li><label for="present-address">Present Address:</label> <input id="present-address" type="text" /></li>
 				<li><label for="permanent-address">Permanent Address:</label> <input id="permanent-address" type="text" /></li>
 				<li><label for="religion">Religion:</label> <input id="religion" type="text" /></li>
 				
 				<li><label for="blood">Blood group:</label> <input id="blood" type="text" /></li>
 				<li><label for="marital-status">Marital Status:</label> <input id="marital-status" type="text" /></li>
 				 -->
-			</ul>
-			<div class="flex" style="gap: 20px;">
-				<button class="primary">Submit</button> <button class="secondary">Clear</button>
-			</div>
+				</ul>
+
+				<div class="flex" style="gap: 20px;">
+					<button type="submit" class="primary">Submit</button>
+					<button class="secondary">Clear</button>
+				</div>
+			</form>
 		</div>
 	</div>
 </div>
+
+{#if selectedEmployee}
+	<div class="modal-container">
+		<div class="modal" style="position:relative; width: 700px">
+			<button
+				style="position: absolute; top:0; right: 0;"
+				on:click={() => (selectedEmployee = null)}
+				><Icon icon="iconamoon:close-duotone" width="24" height="24" /></button
+			>
+			<div
+				class="flex"
+				style="align-items: center; gap:20px; justify-content: space-between; padding-top:15px"
+			>
+				<div style="display:flex; flex-direction:column; gap:0px; margin-right:10px">
+					<div class="text-primary" style="font-size: 35px;">{selectedEmployee.name}</div>
+					HR Manager
+				</div>
+				<div class="flex" style="gap: 10px;">
+					<button class="primary">Edit</button>
+					<button class="secondary" on:click={() => goto(`?delete=${selectedEmployee!.id}`)}
+						>Delete</button
+					>
+				</div>
+			</div>
+			<hr />
+
+			<div class="flex" style="gap:20px">
+				<img src="/demo.jpg" style="border:1px solid #eee; border-radius:5px;" alt="" width="100px" height="110px">
+				<ul style="display:flex; flex-direction:column; gap:10px; list-style:none">
+					<li class="flex"><span class="text-primary" style="width:120px;">Date of birth:</span> 15 July 2004</li>
+					<li class="flex"><span class="text-primary" style="width:120px;">Contact No:</span> +88 01759-225437</li>
+					<li class="flex"><span class="text-primary" style="width:120px;">Department:</span> Human Resource</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+{/if}
